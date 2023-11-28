4d4c01b1ce31622145b66110ef4c04d0317e5785
Maria Mesimeri <mmesim@gmail.com>
2023-11-28 13:46:38 +0100

    Organize output structures

--- s_p_ratios.m
+++ s_p_ratios.m
@@ -34,20 +34,33 @@ disp('Preprocessing....')
 disp('Calculate Amplitudes...')
 [filtDataZ,filtDataE,filtDataN]=sp_ratio_cal(filtDataZ,filtDataE,filtDataN,P_Apick_win,P_Bpick_win,S_Apick_win,S_Bpick_win,Noise_win);
 
-%% 05. Rename structure
-newstructure=sprintf('%s_Z',stations{i,1});
-eval([newstructure '=  filtDataZ']); 
+%% 05. Save structure
+%Remove fileds that contain waveforms
+fields={'waveform','wav_proc'};
+filtDataZ=rmfield(filtDataZ,fields);
+filtDataE=rmfield(filtDataE,fields);
+filtDataN=rmfield(filtDataN,fields);
 
-newstructure=sprintf('%s_E',stations{i,1});
-eval([newstructure '=  filtDataE']);
+%Rename structures based on station name
+% Z
+ newstructure=sprintf('%s_Z',stations{i,1});
+ eval([newstructure '=  filtDataZ']); 
+% E
+ newstructure=sprintf('%s_E',stations{i,1});
+ eval([newstructure '=  filtDataE']);
+% N
+ newstructure=sprintf('%s_N',stations{i,1});
+ eval([newstructure '=  filtDataN']);
 
-newstructure=sprintf('%s_N',stations{i,1});
-eval([newstructure '=  filtDataN']);
+ clear filtDataZ filtDataE filtDataN
+
+% save remaining structures
+filename=sprintf('%s.mat',stations{i,1});
+save(filename)
+ 
 end
 
 %% 06. Shutdown parallel pool
 delete(gcp)
 fprintf('Elapsed time %6.2f minutes... \n',toc/60) %stop timer
 
-
-save results.mat
