d4f3fc03dc29a3d8e867fca3522fc5007a01b37e
Maria Mesimeri <mmesim@gmail.com>
2023-11-28 15:03:36 +0100

    Microbug fixing related to window lengths

--- s_p_ratios.m
+++ s_p_ratios.m
@@ -13,7 +13,7 @@ addpath(genpath(pdir)); %add all *.m scripts to path
 stations=my_setup(workers,mydata);
 %--------------------------------------------------------------------------
 %% 01. Load data (Sac files)
-for i=6%:length(stations) %loop through stations
+for i=5:length(stations) %loop through stations
 disp(stations(i))    
 disp('Loading files..')
 [dataZ,dataE,dataN]=my_loadfiles(mydata,stations{i,1},wlen);
--- src/sp_ratio_cal.m
+++ src/sp_ratio_cal.m
@@ -9,7 +9,7 @@ Samp=Pamp; Namp=Pamp; SP=Pamp; SNR=Pamp;
 if ~isempty(filtDataZ) && ~isempty(filtDataZ) && ~isempty(filtDataZ)
 
 %% Calculate amplitudes
-for i=1:length(filtDataZ)
+parfor i=1:length(filtDataZ)
 % 00. Extract vector from structure
 yZ=filtDataZ(i).wav_proc;    
 yE=filtDataE(i).wav_proc; 
@@ -20,11 +20,6 @@ yN=filtDataN(i).wav_proc;
 % distance is small
 Pstart=round((filtDataZ(i).A-P_Bpick_win)./filtDataZ(i).DELTA); %window before P pick
 Sstart=round((filtDataE(i).T0-S_Bpick_win)./filtDataZ(i).DELTA); %window before S pick
-
-if Pstart<0
-    Pstart=1;
-end
-
 
 Pstop=round((filtDataZ(i).A+P_Apick_win)./filtDataZ(i).DELTA); %window after P pick
 Sstop=round((filtDataE(i).T0+S_Apick_win)./filtDataZ(i).DELTA); %window after S pick;
@@ -38,21 +33,39 @@ Nstart=Pstart-(Noise_win./filtDataZ(i).DELTA); %window after pick;
 Nstop=Pstart; %Noise window stops when P window starts 
 
 %Now check noise window
-if Nstart>0
+if Nstart>0 && Nstop<length(filtDataE(i).waveform) && Nstart<length(filtDataE(i).waveform)
 
-    Namp(i,1)=my_amplitudes(fix(Nstart),fix(Nstop),yZ,yE,yN);
+    Namp(i,1)=my_amplitudes(round(Nstart),round(Nstop),yZ,yE,yN);
 
-else
+else 
     Namp(i,1)=1;
 end
 
+%Now check P  windows
+if Pstart<=0 
+    Pstart=1;
+    Pamp(i,1)=my_amplitudes(round(Pstart),round(Pstop),yZ,yE,yN);
+    
+elseif Pstart>length(filtDataE(i).waveform) || Pstop>length(filtDataE(i).waveform)
+    
+    Pamp(i,1)=-9;
+    
+elseif     Pstart<length(filtDataE(i).waveform) && Pstop<length(filtDataE(i).waveform)
+    Pamp(i,1)=my_amplitudes(round(Pstart),round(Pstop),yZ,yE,yN);
+end
 
-%Compute Amplitudes
 
-Pamp(i,1)=my_amplitudes(fix(Pstart),fix(Pstop),yZ,yE,yN);
+%Now check S windows
+if  Sstart>length(filtDataE(i).waveform) || Sstop>length(filtDataE(i).waveform)
+    
+    Samp(i,1)=-9;
+    
+elseif Sstart<length(filtDataE(i).waveform) && Sstop<length(filtDataE(i).waveform)
+    
+    Samp(i,1)=my_amplitudes(round(Sstart),round(Sstop),yZ,yE,yN);
+end
 
-Samp(i,1)=my_amplitudes(fix(Sstart),fix(Sstop),yZ,yE,yN);
-
+%Get ratios  Amplitudes
 SNR(i,1)=Pamp(i,1)./Namp(i,1);
 
 SP(i,1)=Samp(i,1)./Pamp(i,1);
